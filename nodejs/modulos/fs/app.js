029f7e7664f9cb00f9536e416c191ba20d73ae28
Valentina Branco Daniel <valentinabrancodaniel@gmail.com>
2025-09-02 09:27:03 -0300

    versão módulos os e fs

---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs') ;
+
+fs.readFile('example.txt','utf8', (erro, conteudo_d0_arquivo ) => {
+    if (erro) {
+        console.error('Erro ao ler o arquivo:', err0);
+        return;
+    }
+    console.log('Conteúdo do arquivo:', conteudo_d0_arquivo);
+  });
+
+  
+console.log('Esta mensagem aparece primeiro.');
